d6c199d7ce7340d38679adc2251df35f57c5cf70
bhatsushanth <bhatsushanth@gmail.com>
2012-09-05 10:21:15 +0000

    nAryTree_with_file_impl

--- src/nAryTree.c
+++ src/nAryTree.c
@@ -1,65 +1,37 @@
 #include "nAryTree.h"
-//#include "vfs.h"
-#define MAX_NAME 10
-#define MAX_DIR 10
-#define TRUE 1
-#define FALSE 0
 #include<stdio.h>
 #include<stdlib.h>
 #include<string.h>
-//char tokens[MAX_DIR][MAX_NAME];
+#define TRUE 1
+#define FALSE 0
+#define MAX_NAME 10
+#define MAX_DIR 10
 
 struct dirNode* getNode()
 {
 	struct dirNode *p;
 	p=(struct dirNode*)malloc(sizeof(struct dirNode));
 	p->firstChild=NULL;
-	p->rightSibling=NULL;
+	p->rightSibling=NULL;	
 }
 
-/*int parsePath(char* path)
-{
-	int l,i,j=0,dec=1,k;
-	char *temp;
-	temp=(char*)malloc(sizeof(MAX_NAME));
-	l=strlen(path);
-	for(i=0;i<l;i++)
-	{		
-		if(dec==1)
-		{
-			k=0;
-			dec=0;
-		}	
-		if(path[i]=='/')	
-		{
-			*(temp+k)='\0';
-			strcpy(tokens[j],temp);		//Copy the token when you get '/'
-			j++;
-			dec=1;
-		}
-		else
-		{
-			*(a+k)=path[i];	//Append to temp until '/' in the path
-			k++;
-		}
-	}
-	*(a+k)='\0';
-	strcpy(*(p+j),a);
-	return ++j;
-}
-*/
-struct dirNode* insertNAry(struct dirNode* root,char *path)
+struct dirNode* insertNAry(fileDescriptor *fd,struct dirNode* root)
 {
 	struct dirNode *p,*parent;
 	char *ref;
 	int exitStatus=FALSE;
 	p=getNode();
-	(p->fileDesc).fullPath=path;
 	if(p==NULL)
 	{
 		printf("Sorry NO SUFFICIENT MEMORY");
 		exit(0);
 	}
+	
+	strcpy(p->fileDesc.fileName,fd->fileName);
+	strcpy(p->fileDesc.fullPath,fd->fullPath);
+	strcpy(p->fileDesc.fileType,fd->fileType);
+	p->fileDesc.fileSize=fd->fileSize;
+	p->fileDesc.locationBlockNo=fd->locationBlockNo;
 	if(root==NULL)
 	{
 		root=p;
@@ -67,12 +39,11 @@ struct dirNode* insertNAry(struct dirNode* root,char *path)
 	}
 	else
 	{
+		
 			parent=root;
 			while(exitStatus==FALSE)
 			{
-				puts(path);
-				puts(parent->fileDesc.fullPath);	
-				if(strstr(path,(parent->fileDesc).fullPath)==NULL)
+				if(strstr(p->fileDesc.fullPath,(parent->fileDesc).fullPath)==NULL)
 				{
 					if(parent->rightSibling==NULL)
 					{
@@ -99,41 +70,69 @@ struct dirNode* insertNAry(struct dirNode* root,char *path)
       return root;
 }
 
-void displayNAry(struct dirNode* root)
+void displayNAry(struct dirNode *root)
 {
-	struct dirNode* parent=root;
-	while(parent!=NULL)
+	if(root!=NULL)
 	{
-		puts((parent->fileDesc).fullPath);
-		if(parent->rightSibling==NULL)
-			parent=parent->firstChild;
-		else
-			parent=parent->rightSibling;
-	}		
+		puts(root->fileDesc.fullPath);
+		displayNAry(root->rightSibling);	//Pre-order traversal
+		displayNAry(root->firstChild);
+	}
+			
 }
+
+void saveToFile(struct dirNode *root,FILE *fstore)
+{
+	if(root!=NULL)
+	{
+		fwrite(&(root->fileDesc),sizeof(fileDescriptor),1,fstore);
+		saveToFile(root->rightSibling,fstore);
+		saveToFile(root->firstChild,fstore);
+	}
 	
+}
+
 int main()
 {
 	int i;
-	//fileDescriptor *fd;
-	//fd=(fileDescriptor*)malloc(sizeof(fileDescriptor));
-	char fullPath[20];
+	char ch;
+	FILE *fp,*fstore,*fp2;
 	struct dirNode* root=NULL;
-	for(i=0;i<5;i++)
+	fileDescriptor *fd;
+	fstore=fopen("nary_store.dat","wb");
+	fd=(fileDescriptor*)malloc(sizeof(fileDescriptor));
+	if((fp=fopen("inputfiledesc.txt","r"))==NULL)
+		printf("Cannot open file");
+	else
 	{
-		printf("Enter filename,fullpath,filetype,filesize,blk_no");
-	//	scanf("%s",fd->fileName);
-	//	scanf("%s",fd->fullPath);
-	//	scanf("%s",fd->fileType);
-	//	scanf("%ld",&fd->fileSize);
-	//	scanf("%d",&fd->locationBlockNo);
-		scanf("%s",fullPath);
-		root=insertNAry(root,fullPath);
+		while(fscanf(fp,"%s",fd->fileName)!=EOF)
+		{
+			fscanf(fp,"%s",fd->fullPath);
+			fscanf(fp,"%s",fd->fileType);
+			fscanf(fp,"%ld",&fd->fileSize);
+			fscanf(fp,"%d",&fd->locationBlockNo);
+			root=insertNAry(fd,root);
+		}
+	}
+	fclose(fp);
+	//displayNAry(root);
+	saveToFile(root,fstore);
+	fclose(fstore);	
+	root=NULL;
+	if((fp2=fopen("nary_store.dat","rb"))==NULL)
+		printf("Cannot open file");
+	else
+	{
+		while(fread(fd,sizeof(fileDescriptor),1,fp2))
+		{			
+			root=insertNAry(fd,root);
+		}
 	}
 	displayNAry(root);
-	/*int l=parsePath("D:/Sush/hat");
-	for(i=0;i<l;i++)
-		puts(*(p+i));*/
+	fclose(fp);
+	
 	return 0;
-			
 }
+
+
+
