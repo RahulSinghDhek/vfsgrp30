053e0b48729cf0d50a458b3d3227eb02aa133f80
bhatsushanth <bhatsushanth@gmail.com>
2012-09-05 08:12:19 +0000

    modified

--- include/vfs.h
+++ include/vfs.h
@@ -3,30 +3,32 @@
 #define MAX_FULL_PATH_SIZE 100
 #define MAX_FILE_TYPE_SIZE 10
 #define MAX_BUFFER_SIZE 100
+#define MAX_NO_OF_FILE_DESC 10
+#define MAX_FREE_LIST_SIZE 20
 
 struct mainHeader{
 	char fileSystemLabel[MAX_FILE_SYSTEM_LABLE_SIZE];
 	int maxNoOfFileDescriptors;
 	int noOfFileDescriptors;
-	struct fileDescriptor *fdHead;
-	struct freeList *listHead;
+	fileDescriptor filedesc[MAX_NO_OF_FILE_DESC];
+	freeList freelist[MAX_FREE_LIST_SIZE];
 };
 
 
-struct fileDescriptor{
+typedef struct fileDes{
 	char fileName[MAX_FILE_NAME_SIZE];
 	char fullPath[MAX_FULL_PATH_SIZE];
 	char fileType[MAX_FILE_TYPE_SIZE];
 	long fileSize;
 	int locationBlockNo;
-};
+}fileDescriptor;
 
-struct freeList{
+typedef struct freeLis{
 	int blockNo;
 	struct feeList *next;
-};
+}freeList;
 
 struct block{
-	int buffer[MAX_BUFFER_SIZE];
+	unsigned char* buffer[MAX_BUFFER_SIZE];
 };
 
