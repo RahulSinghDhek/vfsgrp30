02a6b6aec07cdc2a0b9be31d8641a5503313464e
rahulsinghdhek@gmail.com <rahulsinghdhek@gmail.com@abf9c75e-963f-ba1e-3dff-4dcb359639a3>
2012-09-10 13:45:20 +0000

    all the declarations moved to this file

--- trunk/include/binarySearchTree.h
+++ trunk/include/binarySearchTree.h
@@ -1,7 +1,18 @@
-struct bstNnode{
-	int data;
-	struct bstNode *leftChild;
-	struct bstNode *rightChild;
+#define path_size 50
+
+struct node
+{
+	char filepath[path_size];
+	struct node * left;
+	struct node * right;
 };
 
-void output(char);
+typedef struct node node;
+
+
+node * insert(node * root, char filepath[]);
+void inorder(node *root);
+node * getNode();
+void delete(node *root, char file[]);
+void search(node *root, char filepath[]);
+node * min(node *);
