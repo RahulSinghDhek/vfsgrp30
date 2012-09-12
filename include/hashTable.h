0ccda8475d82c4643b94504bfc2c2e66eec828db
vikasverma155 <vikasverma155@gmail.com>
2012-09-12 06:07:01 +0000

    Updated Hash.h

--- include/hashTable.h
+++ include/hashTable.h
@@ -1,3 +1,22 @@
-#include "linkedList.h"
-#define SIZE 100
-struct node *table[SIZE];
+#define HASH_TAB 27;
+#define FILE_NAME_SIZE 100;
+#define FILE_PATH 1000;
+
+
+struct hash_link
+{	char name[FILE_NAME_SIZE];
+	char fullPath[FILE_PATH];
+	struct hash_link *next;
+};
+
+struct hash_link *array[HASH_TAB]={};				
+
+struct hash_link *insertnode(struct hash_link*, char, char);
+
+int fun_Hash(char);
+
+void searchFile(struct hash_link *, char);
+
+void serchStartPoint(char);
+
+void display_Hash();
