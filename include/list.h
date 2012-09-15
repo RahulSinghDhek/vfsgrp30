6f7cddff4699e93a12d7a12fd4b3d25d0d990d28
srinivas20500 <srinivas20500@gmail.com>
2012-09-15 07:01:23 +0000

    included  linked list header file

---
+++ include/list.h
@@ -0,0 +1,14 @@
+typedef struct node  {
+	void *data;
+	struct node *next;	
+} NODE;
+
+
+NODE *create(void *data);
+NODE *insert_after(NODE *node, void *data);
+NODE *insert_beginning(NODE *start, void *data);
+void delete(NODE *start, NODE *node);
+void printnode(NODE *p, void (*f)(void *));
+void printstr(void *str);
+void printint(void *n);
+void printchar(void *c);
