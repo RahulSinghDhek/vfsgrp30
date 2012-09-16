a1761a8716cf5f91b8721e20e8a133e435863764
vikasverma155@gmail.com <vikasverma155@gmail.com@abf9c75e-963f-ba1e-3dff-4dcb359639a3>
2012-09-16 05:36:34 +0000

    Header File

--- trunk/include/hashTable.h
+++ trunk/include/hashTable.h
@@ -1,6 +1,7 @@
-#define HASH_TAB 27;
-#define FILE_NAME_SIZE 100;
-#define FILE_PATH 1000;
+										//In struct, name is of fixed size,
+#define HASH_TAB 27
+#define FILE_NAME_SIZE 100
+#define FILE_PATH 1000
 
 
 struct hash_link
@@ -9,14 +10,14 @@ struct hash_link
 	struct hash_link *next;
 };
 
-struct hash_link *array[HASH_TAB]={};				
+struct hash_link *array[HASH_TAB];				
 
-struct hash_link *insertnode(struct hash_link*, char, char);
+struct hash_link *insertnode(struct hash_link*, char [], char []);
 
 int fun_Hash(char);
 
-void searchFile(struct hash_link *, char);
+void searchFile(struct hash_link *, char[]);
 
-void serchStartPoint(char);
+void serchStartPoint(char[]);
 
 void display_Hash();
