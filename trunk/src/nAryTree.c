c8fcc1aead5ace05a2c26aea241c61cb805beb7d
bhatsushanth@gmail.com <bhatsushanth@gmail.com@abf9c75e-963f-ba1e-3dff-4dcb359639a3>
2012-09-23 07:07:55 +0000

    nary

--- trunk/src/nAryTree.c
+++ trunk/src/nAryTree.c
@@ -15,35 +15,126 @@ struct dirNode* getNode()
 	p->rightSibling=NULL;	
 }
 
-struct dirNode* insertNAry(FileDescriptor *fd,struct dirNode* root)
+char* parsePath(char *path)
 {
-	struct dirNode *p,*parent;
+	int l,i,j=0,dec=1,k;
+	char *temp;
+	char *pat;
+	pat=(char*)malloc(sizeof(MAX_FULL_PATH_SIZE));
+	char tokens[MAX_DIR][MAX_FULL_PATH_SIZE];
+	temp=(char*)malloc(sizeof(10));			//CHAGE IT
+	l=strlen(path);
+	for(i=1;i<l;i++)
+	{		
+		if(dec==1)
+		{
+			k=0;
+			dec=0;
+		}	
+		if(path[i]=='/')	
+		{
+			*(temp+k)='\0';
+			strcpy(tokens[j],temp);		//Copy the token when you get '/'
+			j++;
+			dec=1;
+		}
+		else
+		{
+			*(temp+k)=path[i];	//Append to temp until '/' in the path
+			k++;
+		}
+	}
+	*(temp+k)='\0';
+	strcpy(tokens[j],temp);
+	strcat(pat,"/");
+	for(i=0;i<j;i++){
+		strcat(pat,tokens[i]);
+		strcat(pat,"/");
+	}
+	return pat;
+}
+struct dirNode* isValidPath(FileDescriptor *fd,struct dirNode* root,int *flag)
+{
+	int l,exitStatus;
+	struct dirNode *parent;
+	char temp_path[MAX_FULL_PATH_SIZE];
+	char *path_part;
+	path_part==(char*)malloc(sizeof(MAX_FULL_PATH_SIZE));	
+	strcpy(temp_path,fd->fullPath);
+	//strcpy(path_part,parsePath(temp_path));
+	path_part=parsePath(temp_path);
+	if(root==NULL)
+	{
+		if(strcmp(path_part,"/")!=0)		//for root path
+			*flag=101;			//wrong path trying to insert at root
+	}
+	else
+	{
+		exitStatus=FALSE;
+		parent=root;
+		while(exitStatus==FALSE)
+		{
+			if(strstr(path_part,parent->fileDesc->fullPath)==NULL)
+			{
+				if(parent->rightSibling==NULL)
+				{
+					if(strncmp(path_part,parent->fileDesc->fullPath,strlen(path_part)-1)!=0)
+						*flag=PATH_NOT_FOUND;
+						exitStatus=TRUE;
+				}
+				else
+						parent=parent->rightSibling;
+			}
+			else
+			{
+				if(parent->firstChild==NULL)
+				{
+					if(strncmp(path_part,parent->fileDesc->fullPath,strlen(path_part)-1)!=0)
+						*flag=PATH_NOT_FOUND;
+					exitStatus=TRUE;
+				}	
+				else
+				{
+					parent=parent->firstChild;
+				}	
+			}
+		}
+	   }	
+     return root;		
+}
+
+struct dirNode* insertNAry(FileDescriptor *fd,struct dirNode* root,int *flag)
+{
+	struct dirNode *p;
+	struct dirNode *parent;
 	char *ref;
 	int exitStatus=FALSE;
 	p=getNode();
 	if(p==NULL)
 	{
 		printf("Sorry NO SUFFICIENT MEMORY");
+		*flag=NOT_ENOUGH_MEMORY;
 		exit(0);
 	}
-	
-	strcpy(p->fileDesc.fileName,fd->fileName);
-	strcpy(p->fileDesc.fullPath,fd->fullPath);
-	strcpy(p->fileDesc.fileType,fd->fileType);
-	p->fileDesc.fileSize=fd->fileSize;
-	p->fileDesc.locationBlockNo=fd->locationBlockNo;
-	if(root==NULL)
-	{
-		root=p;
-		exitStatus=TRUE;
-	}
-	else
-	{
-		
+	//printf("before");
+	root=isValidPath(fd,root,flag);
+	//printf("after");
+	if(*flag==101)
+		printf("\nwrong path entered\n");
+	if(*flag==100)
+	{	
+		p->fileDesc=fd;
+		if(root==NULL)
+		{
+			root=p;
+			exitStatus=TRUE;
+		}
+		else
+		{
 			parent=root;
 			while(exitStatus==FALSE)
 			{
-				if(strstr(p->fileDesc.fullPath,(parent->fileDesc).fullPath)==NULL)
+				if(strstr(p->fileDesc->fullPath,parent->fileDesc->fullPath)==NULL)
 				{
 					if(parent->rightSibling==NULL)
 					{
@@ -66,20 +157,21 @@ struct dirNode* insertNAry(FileDescriptor *fd,struct dirNode* root)
 					}	
 				}
 			}
+		}
 	  }
       return root;
 }
 
-/*void displayNAry(struct dirNode *root)
+void displayNAry(struct dirNode *root)
 {
 	if(root!=NULL)
 	{
-		puts(root->fileDesc.fullPath);
+		puts(root->fileDesc->fullPath);
 		displayNAry(root->rightSibling);	//Pre-order traversal
 		displayNAry(root->firstChild);
 	}
 			
-}*/
+}
 
 void saveToFile(struct dirNode *root,FILE *fstore)
 {
@@ -91,48 +183,57 @@ void saveToFile(struct dirNode *root,FILE *fstore)
 	}
 	
 }
-
 /*int main()
 {
 	int i;
 	char ch;
+	int *flag,f=100;
+	flag=(int*)malloc(sizeof(int));
+	flag=&f;
 	FILE *fp,*fstore,*fp2;
 	struct dirNode* root=NULL;
 	FileDescriptor *fd;
 	fstore=fopen("nary_store.dat","wb");
-	fd=(FileDescriptor*)malloc(sizeof(FileDescriptor));
+	//fd=(FileDescriptor*)malloc(sizeof(FileDescriptor));
 	if((fp=fopen("inputfiledesc.txt","r"))==NULL)
 		printf("Cannot open file");
+	
 	else
 	{
-		while(fscanf(fp,"%s",fd->fileName)!=EOF)
+		fd=(FileDescriptor*)malloc(sizeof(FileDescriptor));
+		while(fscanf(fp,"%s",fd->fileName)!=EOF && *flag==100)
 		{
 			fscanf(fp,"%s",fd->fullPath);
 			fscanf(fp,"%s",fd->fileType);
 			fscanf(fp,"%ld",&fd->fileSize);
 			fscanf(fp,"%d",&fd->locationBlockNo);
-			root=insertNAry(fd,root);
+			root=insertNAry(fd,root,flag);
+			printf("%d\n",*(flag));
+			fd=(FileDescriptor*)malloc(sizeof(FileDescriptor));
 		}
 	}
 	fclose(fp);
 	//displayNAry(root);
 	saveToFile(root,fstore);
 	fclose(fstore);	
-	root=NULL;
+	/*root=NULL;
 	if((fp2=fopen("nary_store.dat","rb"))==NULL)
 		printf("Cannot open file");
 	else
 	{
+		fd=(FileDescriptor*)malloc(sizeof(FileDescriptor));
 		while(fread(fd,sizeof(FileDescriptor),1,fp2))
 		{			
 			root=insertNAry(fd,root);
+			fd=(FileDescriptor*)malloc(sizeof(FileDescriptor));
+			//printf("%s",root->fileDesc->fullPath);
 		}
 	}
 	displayNAry(root);
-	fclose(fp);
+	fclose(fp2);*/
 	
-	return 0;
-}*/
+//	return 0;
+//}
 
 
-*/
+
