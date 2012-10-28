2be563131ff0841b7e47e2cb9fcd8fa16c7e5189
bhatsushanth <bhatsushanth@gmail.com>
2012-10-28 10:40:53 +0000

    with_deletedir

--- include/commands.h
+++ include/commands.h
@@ -3,3 +3,7 @@ void create_vfs(char[],long);
 void mount_vfs(char[]);
 void unmount_vfs(char[]);
 void make_dir(char path[],char dir[]);
+void listdirs(char[],int,char[]);
+void addFile(char[], char[], char[]);
+//char* move_dir (char source_dir_path[],char dest_dir_path[]);
+void delete_dir(char path[]);
