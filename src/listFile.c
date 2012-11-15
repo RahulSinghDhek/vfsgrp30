1e3d68d9fdf2c55a2697e9034554239f5479cca4
bhatsushanth <bhatsushanth@gmail.com>
2012-11-15 11:14:40 +0000

    listfile

--- src/listFile.c
+++ src/listFile.c
@@ -8,11 +8,11 @@
 
 void listFile ( char filePath[], char outputFile[]  )
 {
-	
+	printf("\nI m Here inside here.....from raju\n");
 	struct BSTnode *Node;
 	FileDescriptor *fd;
-	FILE *fp;
-	int i,blockNo;
+	FILE *fp,*fp1;
+	int i,blockNo,j;
 	char label[MAX_FILE_SYSTEM_LABLE_SIZE];
 	int size;
 	char str[MAX_BLOCK_SIZE];
@@ -26,6 +26,7 @@ void listFile ( char filePath[], char outputFile[]  )
 		blockNo=Node->filedesc->locationBlockNo;
 		size=Node->filedesc->fileSize;
 		printf("\n the block no is:%d",blockNo);
+		printf("\n the size is:%d",size);
 	}
 	else
 	{
@@ -37,20 +38,39 @@ void listFile ( char filePath[], char outputFile[]  )
 	}
 	else
 	{
-		fseek(fp,sizeof(MetaHeader),SEEK_CUR);
+		printf("\nI m Here inside here.....\n");
+		fseek(fp,sizeof(MetaHeader),SEEK_SET);
 		fseek(fp,blockNo*sizeof(Block),SEEK_CUR);
-		fread(str,size*sizeof(char),1,fp);
+		//fread(str,size*sizeof(char),1,fp);
+			int i=0;
+		printf("size from list file %d\n",size);
+			while(i<=size)
+						{
+						str[i++]=fgetc(fp);
+						printf("%c",str[i-1]);	
+						}
+						//str[i]='\0';						
+
+							
+		str[i-1]='\0';
+
+
+
+
+		
+		
 	}
 	fclose(fp);
-	if((fp=fopen(outputFile,"w+"))==NULL)
+	if((fp1=fopen(outputFile,"w+"))==NULL)
 	{
 		//error reprting
 	}
 	else
 	{
+	
 		printf("\nThe data written to o/p file is:  %s\n",str);
-		fprintf(fp,"%s",str);
+		fprintf(fp1,"%s",str);
 	}
-	fclose(fp);
+	fclose(fp1);
 
 }
