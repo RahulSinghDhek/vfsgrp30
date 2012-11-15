9e9d45ad390b00f50ee1ad5a808695f5af7d7bf5
bhatsushanth <bhatsushanth@gmail.com>
2012-11-15 11:16:14 +0000

    unmount

--- src/unmount_vfs.c
+++ src/unmount_vfs.c
@@ -10,26 +10,45 @@ void unmount_vfs(char label[])
 	//printf("%s unmounted\n",label);
 	char temp[TEMP_SIZE];	
 	FileDescriptor filedescArray[MAX_NO_OF_FILE_DESCRIPTORS];
-	strcpy(temp,label);
-	strcat(temp,".dat");	//add path
+	//strcpy(temp,label);
+	//strcat(temp,".dat");	//add path
 	int i,count=0;
 	//MetaHeader metaHeader;
-	FILE *fp;
-	if((fp=fopen(temp,"rb+"))==NULL)		//Open binary file in write mode
+	//FILE *fp;
+	/*if((fp=fopen(temp,"rb+"))==NULL)		//Open binary file in write mode
 	{	
 		printf("Cannot create file");
 		flag=CANNOT_CREATE_FILE;
-	}
-	else
-	{
+	}*/
+	//else
+	//{
+
+
+		if(flag!=200)
+		{
+			printf(ERR_VFS_UNMOUNT_02);
+			flag=302;
+		}
+		else if(strcmp(mhd.fileSystemLabel,label)!=0)
+			{
+				printf(ERR_VFS_UNMOUNT_01);
+				flag=301;
+			}		
+		else
+		{
+		flag=300;	
 		saveToArray(naryRoot,filedescArray,&count);
 		//strcpy(mhd.fileSystemLabel,label);
 		mhd.noOfFileDescriptors=count;
 		for(i=0;i<count;i++){
 				mhd.filedescArray[i]=filedescArray[i];
 		}		
-		fwrite(&mhd,sizeof(MetaHeader),1,fp);
-		
+		if(fwrite(&mhd,sizeof(MetaHeader),1,vfs_file)==0)
+		{
+			printf(ERR_VFS_UNMOUNT_03);
+			flag=303;
+		}
 	}
+	//fclose(vfs_file);
 }
 	
