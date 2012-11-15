e8c13975931aa3db7f9ed9f8d315eae0c9c06b58
bhatsushanth@gmail.com <bhatsushanth@gmail.com@abf9c75e-963f-ba1e-3dff-4dcb359639a3>
2012-11-15 11:14:23 +0000

    listdir

--- trunk/src/list_dir.c
+++ trunk/src/list_dir.c
@@ -6,6 +6,8 @@
 #include "vfs.h"
 #include "nAryTree.h"
 
+
+struct dirNode *tempRoot=NULL;
 void displayNonRecursive(struct dirNode* root,FILE *fp)
 {
 	if(root!=NULL)
@@ -28,13 +30,40 @@ void displayRecursive(struct dirNode* root,FILE *fp)
 struct dirNode* searchNary(struct dirNode* root,char src_path[])
 {
 	if(root!=NULL){
-		if(strcmp(root->fileDesc->fullPath,src_path)==0)
-			return root;
+		if(strcmp(root->fileDesc->fullPath,src_path)==0){
+			printf("%s",root->fileDesc->fileName);
+			tempRoot=root;
+		}
 		searchNary(root->rightSibling,src_path);
 		searchNary(root->firstChild,src_path);
-	}
-	return NULL;	
+	}	
 }
+
+/*struct dirNode* searchNary(struct dirNode* root,char source_dir_path[])
+{
+	struct dirNode *curr=NULL;
+	curr = root;
+	int exitStatus;
+	struct dirNode *parent;
+	
+		exitStatus=FALSE;
+		parent=naryRoot;
+		while(exitStatus==FALSE)
+		{	
+			if(strstr(source_dir_path,parent->fileDesc->fullPath)==NULL)
+			{	parent=parent->rightSibling;
+				curr = parent;	
+			}
+			else
+			{	if(strcmp(source_dir_path,parent->fileDesc->fullPath)==0)
+					return curr;
+				else
+				{	parent=parent->firstChild;
+					curr = parent;
+				}
+			}
+		}
+}*/
 
 void listdirs(char src_path[],int flag,char dest_path[])
 {
@@ -44,18 +73,22 @@ void listdirs(char src_path[],int flag,char dest_path[])
 		printf("Cannot open file");
 	else
 	{
-	struct dirNode *tempRoot;
-	tempRoot=searchNary(naryRoot,src_path);
-	if(tempRoot==NULL)
-		printf("\nNo file found\n");
-	tempRoot=tempRoot->firstChild;
-	if(tempRoot!=NULL&&flag==0)
-		displayNonRecursive(tempRoot,fptr);
-	else
-	if(tempRoot!=NULL&&flag==1)
-		displayRecursive(tempRoot,fptr);
-	else
-		printf("ERROR:Path not found");
+		//struct dirNode *tempRoot;
+		//tempRoot=
+		searchNary(naryRoot,src_path);
+		if(tempRoot==NULL)
+			printf("\nNo file found\n");
+		else
+		{
+			tempRoot=tempRoot->firstChild;
+			if(tempRoot!=NULL&&flag==0)
+				displayNonRecursive(tempRoot,fptr);
+			else
+			if(tempRoot!=NULL&&flag==1)
+				displayRecursive(tempRoot,fptr);
+			else
+				printf("ERROR:Path not found");
+		}
 	}	
 }
 
