715843d0fd41cb6b0fb65af043273db91ef997d9
bhatsushanth <bhatsushanth@gmail.com>
2012-11-15 11:14:56 +0000

    makedir

--- src/makedir.c
+++ src/makedir.c
@@ -11,7 +11,21 @@ void make_dir(char path[],char dir[])
 {
 	FileDescriptor *fd;
 	int index=0;
+	char path1[MAX_FULL_PATH_SIZE];
+	strcpy(path1,path);
+	if(path[0]!='/' || flag!=200)
+	{
+		printf(ERR_VFS_MAKEDIR_01);
+		flag=401;
+	}
+	else if(searchBST(rootBST , strcat(path1,dir) ))
+	{
+		printf(ERR_VFS_MAKEDIR_03);
+		flag=403;
+	}
 	
+else
+{
 	if(strcmp(path,"/")!=0)
 		strcat(path,"/");
 	
@@ -34,7 +48,8 @@ void make_dir(char path[],char dir[])
 	displayNAry(naryRoot);
 	printf("\n BSTree display  :  ");
 	displayBST(rootBST);
-	printf("\n HTable display  :  ");
+	printf("\n HTable display in makedir :  ");
 	display_Hash();
+}
 }
 
