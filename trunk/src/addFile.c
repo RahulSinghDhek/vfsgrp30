9fff0e816b64c0b7978f5d4b14498dac80e19bfd
bhatsushanth@gmail.com <bhatsushanth@gmail.com@abf9c75e-963f-ba1e-3dff-4dcb359639a3>
2012-11-29 17:13:29 +0000

    addfile_final

--- trunk/src/addFile.c
+++ trunk/src/addFile.c
@@ -7,88 +7,115 @@
 #include "binarySearchTree.h"
 #include "hashTable.h"
 
-void addFile(char directoryPath[], char fileName[], char sourceFilePath[])
+int addFile(char directoryPath[], char fileName[], char sourceFilePath[])
 {
 	FILE *fp,*fp1;
 	FileDescriptor *fd;
 	fd=(FileDescriptor*)malloc(sizeof(FileDescriptor));	
-	int index=0;
-	char sourcePath[MAX_FULL_PATH_SIZE];
+	int index=0,check=0;
+	char sourcePath[MAX_FULL_PATH_SIZE],path2[MAX_FULL_PATH_SIZE];;
 	char c;
 	char str[MAX_BLOCK_SIZE];
 	int i,j;
 	char label[MAX_FILE_SYSTEM_LABLE_SIZE];
-	//MetaHeader *md;
-	strcpy(sourcePath,sourceFilePath);
-	strcpy(label,mhd.fileSystemLabel);
-	strcat(label,".dat");
-	printf("label:%s",label);
-	fp = fopen(sourcePath, "r");
-	if(fp==NULL)
-	{
-			printf("ERROR");
-	}
-	str[0]=fgetc(fp);
-	i=0;
-	while(str[i]!=EOF)
-	{
-		str[++i]=fgetc(fp);
-		
-		
-	}
-	str[i]='\0';
-	fclose(fp);
-	printf("str:%s",str);
-	if((fp1=fopen(label,"rb+"))==NULL)
-	{
-		printf("Error in opening abc.dat file\n");
-	}
-	else
-	{
-		fseek(fp1,sizeof(MetaHeader),SEEK_SET);
-		for(j=0;mhd.FreeList[j]!=0;j++);
-		fseek(fp1,j*sizeof(Block),SEEK_CUR);
-		fwrite(str,i*sizeof(char),1,fp1);
-		mhd.FreeList[j]=1;
-	}
-	strcat(directoryPath,"/");
-	strcat(directoryPath,fileName);
-	printf("dirPath:%s",directoryPath);
-	strcpy(fd->fileName,fileName);
-	strcpy(fd->fullPath,directoryPath);
-	strcpy(fd->fileType,"file");
-	fd->fileSize=i;
-	fd->locationBlockNo=j;
-	printf("\n locationBlock no is %d \n ",j);
-	rootBST=insertBST(rootBST,fd,&flag);
-		printf("\n i m in1\n");
-	
-	naryRoot=insertNAry(fd,naryRoot,&flag);
-			printf("\n i m in2\n");
-	index = fun_Hash(fd->fileName[0]);
-			printf("\n i m in3\n");
-	array[index] = insertnode(array[index], fd);
-				printf("\n i m in4\n");
-	//fclose(fp1);
-	printf("\n i m in\n");
+	long sizeOfSource; 
+	strcpy(path2,directoryPath);	
+	strcat(path2,"/");
 
-	//-----------------------------------reading----------------------------------
-	if((fp=fopen(label,"rb+"))==NULL)		//Open binary file in read mode
+	//MetaHeader *md;
+	//FILE *fpp;
+	
+	if(naryRoot == NULL)
+		printf(ERR_VFS_ADDFILE_07);				//VFS_NOT_MOUNTED
+	else if(strstr(fileName,"/"))
+		printf(ERR_VFS_ADDFILE_02);				//INVALID_CHARACTER_IN_FILENAME
+	else if(searchBST(rootBST , strcat(path2,fileName) ))
+		printf(ERR_VFS_ADDFILE_03);				//FILE_ALREADY_EXISTS
+	else if(mhd.noOfFileDescriptors == MAX_NO_OF_FILE_DESCRIPTORS || vfs_size <= blocks_written)
+			printf(ERR_VFS_ADDFILE_04);			//FILE_SYSTEM_FULL		
+	else 
 	{	
-			
-		printf("Cannot mount");
-		flag=CANNOT_CREATE_FILE;
-	}
-	else
-	{
+		strcpy(sourcePath,sourceFilePath);
+		strcpy(label,mhd.fileSystemLabel);
+		strcat(label,".dat");
+		//printf("label:%s",label);
+		fp = fopen(sourcePath, "r");
+		if(fp==NULL)
+		{
+				printf(ERR_VFS_ADDFILE_05);		//CANNOT_WRITE_TO_DATAFILE
+				return check;
+		}
+		fseek(fp, 0, SEEK_END);
+		sizeOfSource = ftell(fp);
+		fseek(fp, 0, SEEK_SET);
+		if(sizeOfSource>1024)
+		{
+			//fclose(fp);
+			printf(ERR_VFS_ADDFILE_06);			//FILE_TOO_LARGE
+				return check;	
+		}
+		str[0]=fgetc(fp);
+		i=0;
+		while(str[i]!=EOF)
+		{
+			str[++i]=fgetc(fp);
 		
-		fseek(fp,sizeof(MetaHeader),SEEK_SET);
-		for(j=0;mhd.FreeList[j]!=1;j++);
-		fseek(fp,j*sizeof(Block),SEEK_CUR);
-		fread(str,i*sizeof(char),1,fp);
-		printf("string read:%s",str);
-		//mhd.FreeList[j]=1;
+		
+		}
+		str[i]='\0';
+		fclose(fp);
+		//printf("str:%s",str);
+		if((fp1=fopen(label,"rb+"))==NULL)
+		{
+			printf("Error in opening abc.dat file\n");
+		}
+		else
+		{
+			fseek(fp1,sizeof(MetaHeader),SEEK_SET);
+			for(j=0;mhd.FreeList[j]!=0;j++);
+			fseek(fp1,j*sizeof(Block),SEEK_CUR);
+			if(!(fwrite(str,i*sizeof(char),1,fp1)))
+			{
+				//fclose(fp1);
+				printf(ERR_VFS_ADDFILE_05);		//CANNOT_WRITE_TO_DATAFILE	
+ 				return check;
+			}				
+			mhd.FreeList[j]=1;
+		}
+		strcat(directoryPath,"/");
+		strcat(directoryPath,fileName);
+	//	printf("dirPath:%s",directoryPath);
+		strcpy(fd->fileName,fileName);
+		strcpy(fd->fullPath,directoryPath);
+		strcpy(fd->fileType,"file");
+		fd->fileSize=i;
+		fd->locationBlockNo=j;
+		//printf("\n locationBlock no is %d \n ",j);
+		blocks_written=j+1;
+		naryRoot=insertNAry(fd,naryRoot,&flag);
+		rootBST=insertBST(rootBST,fd,&flag);
+		index = fun_Hash(fd->fileName[0]);
+		array[index] = insertnode(array[index], fd);
+		check=1;
+		fclose(fp1);
+		//-----------------------------------reading----------------------------------
+		/*if((fp=fopen(label,"rb+"))==NULL)		//Open binary file in read mode
+		{	
+			printf("Cannot mount");
+			flag=CANNOT_CREATE_FILE;
+		}
+		else
+		{
+		
+			fseek(fp1,sizeof(MetaHeader),SEEK_SET);
+			for(j=0;mhd.FreeList[j]!=1;j++);
+			fseek(fp1,j*sizeof(Block),SEEK_SET);
+			fread(str,i*sizeof(char),1,fp1);
+			printf("string read:%s",str);
+			//mhd.FreeList[j]=1;
+		}
+		fclose(fp);*/
 	}
-	//fclose(fp);
+	return check;
 }
 
