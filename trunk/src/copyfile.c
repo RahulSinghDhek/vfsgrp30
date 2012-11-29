bf6c74f9a953f512495029b52430dbc5e8d55122
bhatsushanth@gmail.com <bhatsushanth@gmail.com@abf9c75e-963f-ba1e-3dff-4dcb359639a3>
2012-11-29 17:14:12 +0000

    copyfile_final

--- trunk/src/copyfile.c
+++ trunk/src/copyfile.c
@@ -117,12 +117,13 @@ char* part(char *path)
 
 
 
-void copy_file(char paths1[],char paths2[]){
+int copy_file(char paths1[],char paths2[]){
+	int check=0;
 	
-	//printf("hi");
 	FILE *fp,*fp1;
 	int index=0;char c;	
-	int flag=ERROR_FREE;	
+	int flag=ERROR_FREE;
+	int flag1=ERROR_FREE; int flag2=ERROR_FREE;	
 	char path11[MAX_FULL_PATH_SIZE];
 	char str[MAX_BLOCK_SIZE];
 	strcpy(path11,paths1);
@@ -135,125 +136,87 @@ void copy_file(char paths1[],char paths2[]){
 	FileDescriptor *fd;
 	fd=(FileDescriptor*)malloc(sizeof(FileDescriptor));
         strcpy(fd->fullPath,strcat(path11,"/d")); 
-	// flag=iscorrectPath(fd,naryRoot,flag);
+	flag1=iscorrectPath(fd,naryRoot,flag);
 	strcpy(fd->fullPath,strcat(path22,"/d"));
-        // flag=iscorrectPath(fd,naryRoot,flag);
-	//printf("79");
-	if(flag ==PATH_NOT_FOUND)
+         flag2=iscorrectPath(fd,naryRoot,flag);
+	
+	if(naryRoot == NULL)
+		printf(ERR_VFS_COPYFILE_05);	
+	else if(flag1 ==PATH_NOT_FOUND)
 		{
-			printf(" wrong path, please enter correct path \n\n\n");
+		printf(ERR_VFS_COPYFILE_01);
 		}
-	else
-		{	
-			int i=0;int bno1=10;int size=0;int j=0;
+	else if(flag2==PATH_NOT_FOUND)
+		{
+		printf(ERR_VFS_COPYFILE_02);	
+		}
+	
+	else 
+		{
+			int i=0;int bno1=10;int size=0;int j=0;	char type[10];
 			FileDescriptor *fd1;
-			fd1=(FileDescriptor*)malloc(sizeof(FileDescriptor));
 			
-		
-									
-//printf("92");
-				
-				
-				if((fp1=fopen(label,"rb+"))==NULL)
-					{
-						printf("Error in opening  file\n");
-					}
-					else
-					{
-						
-
-
+			fd1=(FileDescriptor*)malloc(sizeof(FileDescriptor));
+			if((fp1=fopen(label,"rb+"))==NULL)
+				{
+				 printf("Error in opening FILE SYSTEM \n");
+				}
+			else
+				{
 					struct BSTnode *p=(struct BSTnode *)malloc(sizeof(struct BSTnode));
-							p=searchBST(rootBST,paths1);
-						if(p!=NULL){
+					p=searchBST(rootBST,paths1);
+					if(p!=NULL)	
+					{
 						bno1=p->filedesc->locationBlockNo;
 						size=p->filedesc->fileSize;
-                                              	 }
-
-
-
-						/*for(i=0;i<MAX_NO_OF_FILE_DESCRIPTORS;i++)
+						strcpy(type,p->filedesc->fileType);
+						int x=strcmp(type,"file");
+						//printf("%d raju \n",x);
+						//printf("%d",bno1);
+						if(x) 
+						printf(ERR_VFS_COPYFILE_03);
+						else if(x==0)
 						{
+		                                  	fseek(fp1,sizeof(MetaHeader),SEEK_SET);
+							fseek(fp1,bno1*sizeof(Block),SEEK_CUR);
+							str[0]=fgetc(fp1);
+							i=0;
 						
-						
-
-						if(strcmp(mhd.filedescArray[i].fullPath,path1)==0)
-								{
-															
-								bno=mhd.filedescArray[i].locationBlockNo;
-							size=mhd.filedescArray[i].fileSize;
-						}
-						
-						
-						}*/
-						printf(" copy file block %d\n",bno1);
-						printf("size of %d",size);
-						fseek(fp1,sizeof(MetaHeader),SEEK_SET);
-						
-						
-						fseek(fp1,bno1*sizeof(Block),SEEK_CUR);
-
-						//fseek(fp1,-1,SEEK_CUR);	
-						
-						//i=0;
-						//str[0]=fgetc(fp1);
-						
-						//str[i]='\0';
+							while(i<=size)
+								str[++i]=fgetc(fp1);
+							str[i-1]='\0';						    
+							fseek(fp1,sizeof(MetaHeader),SEEK_SET);
+						        for(j=0;mhd.FreeList[j]!=0;j++);
+					
+							fseek(fp1,j*sizeof(Block),SEEK_CUR);
+							fwrite(str,i*sizeof(char),1,fp1);
+							mhd.FreeList[j]=1;
+					
+							char *fileName;
+							fileName=(char*)malloc(sizeof(MAX_FULL_PATH_SIZE));
+							fileName=part(paths1);
+							strcat(paths2,"/");
+							strcat(paths2,fileName);
+							strcpy(fd1->fileName,fileName);
+							strcpy(fd1->fullPath,paths2);
+							strcpy(fd1->fileType,"file");
+							fd1->fileSize=size;
+							fd1->locationBlockNo=j;
+							naryRoot=insertNAry(fd1,naryRoot,&flag);
+							rootBST=insertBST(rootBST,fd1,&flag);
+							index = fun_Hash(fd1->fileName[0]);
+							array[index] = insertnode(array[index], fd1);
+							check=1;
 									
-						//fscanf(fp1,"%s",str);
-
-
-					str[0]=fgetc(fp1);
-					i=0;
-						
-					while(i<=size)
-						{
-						str[++i]=fgetc(fp1);
-							
-								}
-						str[i]='\0';						
-
 					}
-					
-
-				    printf("%s\n",str); 
-					fseek(fp1,sizeof(MetaHeader),SEEK_SET);
-				       for(j=0;mhd.FreeList[j]!=0;j++);
-					fseek(fp1,j*sizeof(Block),SEEK_CUR);
-					fwrite(str,i*sizeof(char),1,fp1);
-					mhd.FreeList[j]=1;
-					
-					char *fileName;
-					fileName=(char*)malloc(sizeof(MAX_FULL_PATH_SIZE));
-					fileName=part(paths1);
-					
-					
-
-
-
-
-					strcat(paths2,"/");
-					strcat(paths2,fileName);
-					
-					strcpy(fd1->fileName,fileName);
-					strcpy(fd1->fullPath,paths2);
-					strcpy(fd1->fileType,"file");
-					//printf("%s",fd1->fullPath);
-					fd1->fileSize=size;
-					fd1->locationBlockNo=j;
-					naryRoot=insertNAry(fd1,naryRoot,&flag);
-					
-					rootBST=insertBST(rootBST,fd1,&flag);
-					
-					index = fun_Hash(fd1->fileName[0]);
-					array[index] = insertnode(array[index], fd1);
-					//fclose(fp1);
-			
-
+				
+				}
+		fclose(fp1);			
 		}
 
 }
-
+return check;
+}
 
 
 
@@ -271,13 +234,13 @@ FileDescriptor *fd2;
 	path_part==(char*)malloc(sizeof(MAX_FULL_PATH_SIZE));	
 	strcpy(temp_path,fd2->fullPath);
 	//strcpy(path_part,parsePath(temp_path));
-//printf("%s\n",temp_path);		
-path_part=parsePath1(temp_path);
-//printf("%s\n",path_part);
+	//printf("%s\n",temp_path);		
+	path_part=parsePath1(temp_path);
+	//printf("%s\n",path_part);
 	if(root==NULL)
 	{
 		if(strcmp(path_part,"/")!=0)		//for root path
-			flag=PATH_NOT_FOUND;			//wrong path trying to insert at root
+			flag=PATH_NOT_FOUND;		//wrong path trying to insert at root
 	}
 	else
 	{
