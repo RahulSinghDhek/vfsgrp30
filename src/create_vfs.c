c2be02b24d02b0b55b101458bd813eeb3b078a3e
bhatsushanth <bhatsushanth@gmail.com>
2012-11-29 17:11:54 +0000

    cretevfs_final

--- src/create_vfs.c
+++ src/create_vfs.c
@@ -1,47 +1,35 @@
+//changed on 28th November,2012
 #include "list.h"
 #include "vfs.h"
 #include "commands.h"
 #include<stdio.h>
 #include<string.h>
 #include<stdlib.h>
-void create_vfs(char label[],long systemSize)
+int create_vfs(char label[],long systemSize)
 {
-	
+	int check = 0;
 	char lb[MAX_FILE_SYSTEM_LABLE_SIZE];	//re-name
-	strcpy(lb,label);
-	strcat(lb,".dat");	//add path
-	int i;
-	FILE *fp;
-	MetaHeader metaHeader;
-	flag=100;		//NO_ERRORS;
+	if(strlen(label) > MAX_FILE_SYSTEM_LABLE_SIZE)
+		printf(ERR_VFS_CREATE_05);			//VFS_LABEL_TOO_LARGE
+	else
+	{
+		strcpy(lb,label);
+		strcat(lb,".dat");	//add path
+		int i;
+		FILE *fp;
+		MetaHeader metaHeader;
+		vfs_size=systemSize;
 	
-	vfs_size=systemSize;
-
-	if(systemSize>MAX_VFS_MEMORY)
-	{
-		flag=103;
-		printf(ERR_VFS_CREATE_03);		
-	}
-	else if(systemSize % MAX_BLOCK_SIZE != 0)
-	{
-		flag=105;
-		printf(ERR_VFS_CREATE_05);		
-	}
-	else if((fp=fopen(lb,"rb"))!=NULL)
-	{	
-		flag=101;
-		printf(ERR_VFS_CREATE_01);
-		fclose(fp);	
-	}
-	else{
-		//fclose(fp);	
-		if((fp=fopen(lb,"wb"))==NULL)
-		{	
-			printf("Cannot create file");
-			//*flag=CANNOT_CREATE_FILE;
-			flag=102;
-			printf(ERR_VFS_CREATE_02);	
+		if((fp=fopen(lb,"rb"))!=NULL)
+		{       printf(ERR_VFS_CREATE_01);			//DATA_FILE_ALREADY_EXISTS
+		        //fclose(fp);    
 		}
+		else if(systemSize>MAX_VFS_MEMORY || systemSize<MIN_VFS_MEMORY)
+		    	printf(ERR_VFS_CREATE_04);              	//INVALID_SIZE
+		else if(strstr(label,"/")!=NULL)
+			printf(ERR_VFS_CREATE_03);			//INVALID_CHARACTER_IN_NAME
+		else if((fp=fopen(lb,"wb"))==NULL)
+			printf(ERR_VFS_CREATE_02);  			//CANNOT_CREATE_DATAFILE		
 		else
 		{
 			FileDescriptor *fdroot;
@@ -55,20 +43,16 @@ void create_vfs(char label[],long systemSize)
 			strcpy(metaHeader.fileSystemLabel,label);
 			metaHeader.noOfFileDescriptors=1;
 			for(i=0;i<MAX_NO_OF_FILE_DESCRIPTORS;i++)
-			{
 				metaHeader.FreeList[i]=0;
-			}
 		
 			fwrite(&metaHeader,sizeof(MetaHeader),1,fp);
-			for(i=0;i<(systemSize/sizeof(Block));i++)
-			{
-				Block block;
+			for(i=0;i<(systemSize);i++)
+			{	Block block;
 				fwrite(&block,sizeof(Block),1,fp);
 			}
+			fclose(fp);
+			check = 1;
 		}
 	}
+	return check;
 }
-/*int main()
-{
-	create_vfs("zzz",1024);
-}*/
